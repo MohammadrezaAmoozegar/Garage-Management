4b9fc78fb400b58290a7dd9cc3865faa13a22f64
Ali Momeni Moghaddam <ammking385@gmail.com>
2025-12-06 20:43:34 +0330

    feat: add ParkingManager with addCarToFirstAvailable method

--- src/model/ParkingManager.java
+++ src/model/ParkingManager.java
@@ -13,4 +13,19 @@ public class ParkingManager{
         }
         queue = new MyQueue();
     }
+    public String addCarToFirstAvailable(int carId){
+        Car car = new Car(carId);
+        if (!queue.isEmpty()){
+            queue.enqueue(car);
+            return "Parking is full. Car added to queue.";
+        }
+        for (int i = 0; i < n; i++){
+            if (!stacks[i].isFull()){
+                stacks[i].push(car);
+                return "Car " + carId + " added to stack " + (i + 1);
+            }
+        }
+        queue.enqueue(car);
+        return "Parking full. Car moved to queue.";
+    }
 }
